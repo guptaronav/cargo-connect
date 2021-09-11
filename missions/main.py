4f7bdf24f9d1925ce019bd2bbbf9b6c0ad66352d
Noblelion9 <86336306+Noblelion9@users.noreply.github.com>
2021-09-11 16:03:31 -0700

    Update main.py

--- missions/main.py
+++ missions/main.py
@@ -2,6 +2,7 @@ from spike import PrimeHub, Motor, Button, StatusLight, ForceSensor, MotionSenso
 from spike.control import wait_for_seconds, wait_until, Timer
 import math
 from math import *
+from spike import MotorPair
 
 # Initialize the Distance Sensor.
 distance = DistanceSensor('D')
@@ -9,8 +10,8 @@ hub= PrimeHub
 # Initialize the Color Sensor
 color = ColorSensor('B')
 left_motor = Motor('C')
-
-from spike import MotorPair
+left_motor.stop()
+left_motor.set_stop_action('coast')
 
 # If the left motor is connected to Port B,
 # and the right motor is connected to Port A.
@@ -18,37 +19,61 @@ motor_pair = MotorPair('A', 'E')
 motor_pair.set_default_speed(40)
 motor_pair.set_stop_action('coast')
 
-# motor_pair.start()
-# move straight from starting box and turn right some angle
-#motor_pair.move_tank(20, 'cm', left_speed=80, right_speed=57)
-#motor_pair.set_motor_rotation(8.25 * math.pi, 'cm')
-# move straight till you get to the bridge at constant speed
+def move_to_black():
+    motor_pair.start()
+    color.wait_until_color('black')
+    motor_pair.stop()
 
+def move_x_bot(distance):
+    motor_pair.start()
+    motor_pair.move_tank(distance, 'cm', left_speed=40, right_speed=40)
+    motor_pair.stop()
 
-# def left_turn_end():
-#         return hub.motion_senor.get_yaw_andle() >90
+def s_move():
+    motor_pair.start()
+    motor_pair.move_tank(6, 'cm', left_speed=15, right_speed=30)
+    motor_pair.stop()
+    motor_pair.start()
+    motor_pair.move_tank(6, 'cm', left_speed=30, right_speed=15)
+    motor_pair.stop()
 
-# wait_until(left_turn_end)
-# motor_pair.stop();
+def go_around():
+    motor_pair.move(65, unit='cm', steering=0)
+    motor_pair.move_tank(20, 'cm', left_speed=20, right_speed=40)
+    motor_pair.move_tank(20, 'cm', left_speed=20, right_speed=40)
+    motor_pair.move(30, unit='cm', steering=0)
+    motor_pair.move_tank(10, 'cm', left_speed=20, right_speed=40)
+    motor_pair.move(45, unit='cm', steering=0)
+    motor_pair.move(50, unit='cm', steering=0, speed=100)
 
-# Robot Go around
-# motor_pair.move(65, unit='cm', steering=0)
-# motor_pair.move_tank(20, 'cm', left_speed=20, right_speed=40)
-# motor_pair.move_tank(20, 'cm', left_speed=20, right_speed=40)
-# motor_pair.move(30, unit='cm', steering=0)
-# motor_pair.move_tank(10, 'cm', left_speed=20, right_speed=40)
-# motor_pair.move(45, unit='cm', steering=0)
+###################### ROUND 1 ###########################
+move_to_black()
 
-motor_pair.move(50, unit='cm', steering=0, speed=100)
-# color.wait_until_color('black')
-# motor_pair.stop();
-# left_motor.run_for_degrees(-290,100)
+#turn robot
+motor_pair.start()
+motor_pair.move_tank(3, 'cm', left_speed=20, right_speed=20)
+motor_pair.move_tank(17, 'cm', left_speed=43, right_speed=20)
 
-# motor_pair.move_tank(10, 'cm', left_speed=40, right_speed=20)
-# motor_pair.start()
-# color.wait_until_color('black')
-# motor_pair.stop();
+# Mission 05: Switch engine (20 points)
+left_motor.start()
+left_motor.run_for_degrees(50,7)
+left_motor.stop()
+motor_pair.move_tank(-18, 'cm', left_speed=45, right_speed=20)
+motor_pair.move_tank(-5, 'cm', left_speed=20, right_speed=20)
+# Make a tank move
+motor_pair.move_tank(10, 'cm', left_speed=40, right_speed=-56)
 
-#distance_sensor.wait_for_distance_close_than(15,'cm')
-#if distance.get_distance_cm()<12:
-    #motor_pair.stop()
+# Mission 03: Unload Cargo Plane (20+10+10)
+left_motor.run_for_seconds(1,70)
+# Raise the right Hand
+left_motor.run_for_degrees(-100,60)    
+
+# Mission 13: Platooning Trucks (10+10+10)
+motor_pair.move_tank(33, 'cm', left_speed=35, right_speed=41)
+motor_pair.move_tank(6, 'cm', left_speed=20, right_speed=20)
+motor_pair.move_tank(-20, 'cm', left_speed=20, right_speed=20)
+
+# S-Move
+s_move()
+move_to_black()
+
