9f161342c11624558924bf2c833751a6ddad0050
Ronav Gupta <75178927+guptaronav@users.noreply.github.com>
2021-09-17 17:01:57 -0700

    Update main.py
    modular code

--- missions/main.py
+++ missions/main.py
@@ -1,20 +1,15 @@
-from spike import PrimeHub, Motor, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike import PrimeHub, LightMatrix, Button, StatusLight, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
-import math
 from math import *
-from spike import MotorPair
 
-# Initialize the Distance Sensor.
-distance = DistanceSensor('D')
 hub= PrimeHub
 # Initialize the Color Sensor
 color = ColorSensor('B')
-left_motor = Motor('C')
-left_motor.stop()
-left_motor.set_stop_action('coast')
 
-# If the left motor is connected to Port B,
-# and the right motor is connected to Port A.
+left_motor = Motor('C')
+left_motor.set_default_speed(40)
+# left_motor.set_stop_action('coast')
+
 motor_pair = MotorPair('A', 'E')
 motor_pair.set_default_speed(40)
 motor_pair.set_stop_action('coast')
@@ -46,34 +41,57 @@ def go_around():
     motor_pair.move(45, unit='cm', steering=0)
     motor_pair.move(50, unit='cm', steering=0, speed=100)
 
+# Mission 05: Switch engine (20 points)
+def mission_05():
+    left_motor.start()
+    left_motor.run_for_degrees(50,7)
+    left_motor.stop()
+
+# Mission 03: Unload Cargo Plane (20+10+10)
+def mission_03():
+    left_motor.start()
+    left_motor.run_for_seconds(1,70)
+    # Move the right Hand forward 
+    left_motor.run_for_degrees(-170,60)
+    left_motor.stop()
+
 ###################### ROUND 1 ###########################
 move_to_black()
 
 #turn robot
 motor_pair.start()
-motor_pair.move_tank(3, 'cm', left_speed=20, right_speed=20)
-motor_pair.move_tank(17, 'cm', left_speed=43, right_speed=20)
+motor_pair.move_tank(4, 'cm', left_speed=20, right_speed=20)
+motor_pair.stop()
+motor_pair.start()
+#motor_pair.move_tank(15, 'cm', left_speed=30, right_speed=20)
+motor_pair.move_tank(312, 'degrees', left_speed=30, right_speed=20)
+motor_pair.stop()
 
-# Mission 05: Switch engine (20 points)
-left_motor.start()
-left_motor.run_for_degrees(50,7)
-left_motor.stop()
-motor_pair.move_tank(-18, 'cm', left_speed=45, right_speed=20)
-motor_pair.move_tank(-5, 'cm', left_speed=20, right_speed=20)
+mission_05()
+
+motor_pair.start()
+# motor_pair.move_tank(-15, 'cm', left_speed=30, right_speed=20)
+motor_pair.move_tank(-312, 'degrees', left_speed=30, right_speed=20)
+motor_pair.stop()
+motor_pair.start()
+motor_pair.move_tank(-6, 'cm', left_speed=30, right_speed=30)
 # Make a tank move
-motor_pair.move_tank(10, 'cm', left_speed=40, right_speed=-56)
+motor_pair.stop()
+motor_pair.start()
+motor_pair.move_tank(9, 'cm', left_speed=40, right_speed=-57)
+motor_pair.stop()
 
-# Mission 03: Unload Cargo Plane (20+10+10)
-left_motor.run_for_seconds(1,70)
-# Raise the right Hand
-left_motor.run_for_degrees(-100,60)    
+mission_03()
 
-# Mission 13: Platooning Trucks (10+10+10)
-motor_pair.move_tank(33, 'cm', left_speed=35, right_speed=41)
-motor_pair.move_tank(6, 'cm', left_speed=20, right_speed=20)
-motor_pair.move_tank(-20, 'cm', left_speed=20, right_speed=20)
+# # # Mission 13: Platooning Trucks (10+10+10)
+# motor_pair.start()
+# motor_pair.move_tank(27, 'cm', left_speed=35, right_speed=40)
+# motor_pair.stop()
+# motor_pair.start()
+# motor_pair.move_tank(8, 'cm', left_speed=30, right_speed=30)
+# # motor_pair.move_tank(-20, 'cm', left_speed=20, right_speed=20)
+# motor_pair.stop()
 
-# S-Move
-s_move()
-move_to_black()
-
+# # S-Move
+# s_move()
+# move_to_black()
